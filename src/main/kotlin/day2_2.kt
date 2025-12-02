55b96b49836360a2ffebfeb9b05c7775f5400906
Fede Esteban <fede.est@gmail.com>
2025-12-02 12:01:40 -0300

    Day 2

---
+++ src/main/kotlin/day2_2.kt
@@ -0,0 +1,19 @@
+import java.io.File
+
+fun main() {
+    File("src/main/kotlin/resources/day2.txt").useLines { lines ->
+        lines.forEach { line ->
+            val result = line.split(",").fold(0L) { sum, range ->
+                val (first, last) = range.split("-")
+                sum + (first.toLong()..last.toLong()).fold(0L) { partialSum, n ->
+                    n.toString().let { repr ->
+                        partialSum + if ((1..repr.length / 2).any { patternSize ->
+                            repr.substring(0, patternSize).repeat(repr.length / patternSize) == repr
+                        }) n else 0
+                    }
+                }
+            }
+            print(result)
+        }
+    }
+}
